996cb34edcb8f57b13a0c178b3a40ccd95d30954
Mikhail <maybeminsk@gmail.com>
2023-10-29 22:55:31 +0300

    Difference done!

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import MainSlider from "./modules/slider/slider-main";
 import MiniSlider from "./modules/slider/slider-mini";
 import VideoPlayer from "./modules/playVideo";
+import Difference from "./modules/difference";
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({container: '.page', btns: '.next', modal: '.hanson'});
@@ -35,4 +36,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const player = new VideoPlayer('.showup .play', '.overlay');
     player.init();
+
+/*     const difference = new Difference('.officerold', '.officernew', '.officer__card-item');
+    difference.init(); */
+    new Difference('.officerold', '.officernew', '.officer__card-item').init(); // можно так, этот экземпляр один раз отработает и больше не пригодится нам
 });
